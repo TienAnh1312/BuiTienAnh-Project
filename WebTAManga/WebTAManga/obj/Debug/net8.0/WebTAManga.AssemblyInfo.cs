f6ebf3df68f4afaa441ac69633facb3d46a970d7
TienAnh1312 <buitienanh13122003@gmail.com>
2025-03-04 14:37:58 +0700

    update comment (chọn sticker), update details (thanh cuộn chapter)

--- WebTAManga/WebTAManga/obj/Debug/net8.0/WebTAManga.AssemblyInfo.cs
+++ WebTAManga/WebTAManga/obj/Debug/net8.0/WebTAManga.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebTAManga")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+87cad3ac305833e7bca4b330ff2eff0fcf86c74b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b36cc520ce46fa2efd6013d7b90b762f3436efe3")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebTAManga")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebTAManga")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
